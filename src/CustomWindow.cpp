bb8c34999823f67e7eebe8231205d00a5782962b
Rich E <reakinator@gmail.com>
2013-03-15 00:15:48 -0400

    … and custom window drawing seems to be making sense

--- src/CustomWindow.cpp
+++ src/CustomWindow.cpp
@@ -1,11 +1,13 @@
 
-
 #include "CustomWindow.h"
 #include "cinder/gl/gl.h"
 #include "cinder/Utilities.h"
 
+#include "cigwen/CinderGwen.h"
+
 using namespace Gwen;
 using namespace ci;
+
 
 GWEN_CONTROL_CONSTRUCTOR( CustomWindow )
 {
@@ -15,28 +17,30 @@ void CustomWindow::Render( Skin::Base* skin )
 {
 	BaseClass::Render( skin );
 
-	ci::Area frame( m_Bounds.x + m_InnerBounds.x, m_Bounds.y + m_InnerBounds.y, m_InnerBounds.w, m_InnerBounds.h );
-	ci::Area bounds( m_Bounds.x, m_Bounds.y, m_Bounds.w, m_Bounds.h );
-	ci::Area innerBounds( m_InnerBounds.x, m_InnerBounds.y, m_InnerBounds.w, m_InnerBounds.h );
+	ci::Rectf frame( m_Bounds.x + m_InnerBounds.x, m_Bounds.y + m_InnerBounds.y, m_InnerBounds.w, m_InnerBounds.h );
+	ci::Rectf bounds( cigwen::fromGwen( m_Bounds ) );
+	ci::Rectf innerBounds( cigwen::fromGwen( m_InnerBounds ) );
+
 	float aspect = (float)m_InnerBounds.w / (float)m_InnerBounds.h;
 
 	mCubeRotation.rotate( Vec3f( 1, 1, 1 ), 0.03f );
 
 	gl::pushMatrices();
 
-
 	gl::translate( frame.x1, frame.y1 );
-	gl::drawString( std::string( "aspect: " ) + ci::toString( aspect ), Vec2f( 10, 10 ), ci::Color::black() );
-	gl::drawString( std::string( "frame: " ) + ci::toString( frame ), Vec2f( 10, 30 ), ci::Color::black() );
-	gl::drawString( std::string( "bounds: " ) + ci::toString( bounds ), Vec2f( 10, 50 ), ci::Color::black() );
-	gl::drawString( std::string( "innerBounds: " ) + ci::toString( innerBounds ), Vec2f( 10, 70 ), ci::Color::black() );
+	float yOffset = 10;
+	float yHeight = 20;
+	gl::drawString( std::string( "aspect: " ) + ci::toString( aspect ), Vec2f( 10, yOffset ), ci::Color::black() );		yOffset += yHeight;
+	gl::drawString( std::string( "frame: " ) + ci::toString( frame ), Vec2f( 10, yOffset ), ci::Color::black() );		yOffset += yHeight;
+	gl::drawString( std::string( "bounds: " ) + ci::toString( bounds ), Vec2f( 10, yOffset ), ci::Color::black() );		yOffset += yHeight;
+	gl::drawString( std::string( "innerBounds: " ) + ci::toString( innerBounds ), Vec2f( 10, yOffset ), ci::Color::black() );		yOffset += yHeight;
+
 	//----------------------
 
 	gl::pushMatrices();
 
-//	gl::color( ci::Color( 0, 1, 0 ) );
-//	gl::translate( 20, 20 );
-//	gl::drawSolidCircle( Vec2f( 50, 50 ), 20 );
+	gl::color( ci::Color( 0, 1, 0 ) );
+	gl::drawSolidCircle( innerBounds.getCenter(), 20 );
 
 //	gl::enableDepthRead();
 
