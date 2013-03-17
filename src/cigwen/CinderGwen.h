55daff8f28bfd95705f0534dcfcc8b831ec2b624
Rich E <reakinator@gmail.com>
2013-03-17 17:03:05 -0400

    added toGwen / fromGwen for ci::ColorA

--- src/cigwen/CinderGwen.h
+++ src/cigwen/CinderGwen.h
@@ -8,6 +8,9 @@
 
 namespace cigwen {
 
+	inline Gwen::Color toGwen( const ci::ColorA &c )	{ return Gwen::Color( c.r * 255, c.g * 255, c.b * 255, c.a * 255 ); }
+	inline ci::ColorA fromGwen( const Gwen::Color &c )	{ return ci::ColorA( c.r / 255.0f, c.g / 255.0f, c.b / 255.0f, c.a / 255.0f ); }
+
 	inline ci::Vec2f fromGwen( const Gwen::Point &p )	{ return ci::Vec2f( p.x, p.y ); }
 	inline ci::Rectf fromGwen( const Gwen::Rect &r )	{ return ci::Rectf( r.x, r.y, r.x + r.w, r.y + r.h ); }
 
